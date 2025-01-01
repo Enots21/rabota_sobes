441ebdd9cf2a46fc3420a20bf194e54c749dc74f
Енот <68638093+Enots21@users.noreply.github.com>
2025-01-01 15:59:08 +0300

    Задание 2: Модель банковского счета
    Задача: Создайте класс `BankAccount` с атрибутами: `balance` (баланс), `owner` (владелец). Добавьте методы: `__init__` (инициализация с начальным балансом), `deposit` (пополнение счета), `withdraw` (снятие средств), `get_balance` (получение баланса). Методы `deposit` и `withdraw` должны проверять, что баланс не уходит в минус. Обработайте ситуацию, когда пытаются снять больше денег, чем есть на счету (например, выбросите исключение).

---
+++ task23.py
@@ -0,0 +1,40 @@
+class BankAccount:
+    def __init__(self, balance, owner):
+        self.balance = balance
+        self.owner = owner
+        self.deposit()
+        self.withdraw()
+        self.get_balance()
+
+    def deposit(self):  # Пополнение счета
+        dep = input('Введите сумму пополнения: ')
+        if self.balance == 0:
+            self.balance += int(dep)
+            print(f'Пополнение счета на {dep}')
+        else:
+            print("Счет заблокирован")
+
+    def withdraw(self):  # Вывод средств
+        dep = input('Введите сумму снятия: ')
+        if self.balance > 0:
+            self.balance -= int(dep)
+            print('Cнятие со счёт')
+        else:
+            print("Недостаточно средств")
+
+    def get_balance(self):  # Получение баланса
+        print(self.balance)
+
+
+while True:
+    choise = input('Введите действие которые хотите сделать: Пополнить/Вывести/Посмотреть баланс:  ')
+    tom = BankAccount(0, "Tom")
+    if choise == 'Пополнить счет':
+        tom.deposit()
+    elif choise == 'Вывести баланс':
+        tom.withdraw()
+    elif choise == 'Посмотреть баланс':
+        tom.get_balance()
+    else:
+        print('Вы ввели не верное действие')
+    break
